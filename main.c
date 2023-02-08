9fd0eb5d721624a587c28050968fe438f3ae2f2f
Ahmed Hassan <digiator2020@gmail.com>
2023-02-08 13:55:36 +0400

    spaces initialization

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ahassan <ahassan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 15:21:52 by ahassan           #+#    #+#             */
-/*   Updated: 2023/02/08 13:41:23 by ahassan          ###   ########.fr       */
+/*   Updated: 2023/02/08 13:54:29 by ahassan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void map_init(t_map *map)
 	map->exit = mlx_xpm_file_to_image(map->mlx, "includes/exit.xpm", &w, &h);
 }
 
-void draw_map(t_map *map)
+void draw_spaces(t_map *map)
 {
 	map->ypos = 0;
 	map->v = 0;
@@ -36,21 +36,38 @@ void draw_map(t_map *map)
 		map->l = 0;
 		while(map->xpos < map->x)
 		{
-			write(1, &map->map[map->ypos][map->xpos], 1);
+			mlx_put_image_to_window(map->mlx, map->img, map->space, map->l, map->v);
+			map->xpos++;
+			map->l += 64;
+		}
+		write(1, "\n", 1);
+		map->ypos++;
+		map->v += 64;
+	}	
+}
+
+void draw_map(t_map *map)
+{
+	draw_spaces(map);
+	map->ypos = 0;
+	map->v = 0;
+	while (map->ypos < map->y)
+	{
+		map->xpos = 0;
+		map->l = 0;
+		while(map->xpos < map->x)
+		{
 			if(map->map[map->ypos][map->xpos] == 'P')
 				mlx_put_image_to_window(map->mlx, map->img, map->player, map->l, map->v);
 			if(map->map[map->ypos][map->xpos] == 'E')
 				mlx_put_image_to_window(map->mlx, map->img, map->exit, map->l, map->v);
 			if(map->map[map->ypos][map->xpos] == 'C')
 				mlx_put_image_to_window(map->mlx, map->img, map->coin, map->l, map->v);
-			if(map->map[map->ypos][map->xpos] == '0')
-				mlx_put_image_to_window(map->mlx, map->img, map->space, map->l, map->v);
 			if(map->map[map->ypos][map->xpos] == '1')
 				mlx_put_image_to_window(map->mlx, map->img, map->wall, map->l, map->v);
 			map->xpos++;
 			map->l += 64;
 		}
-		write(1, "\n", 1);
 		map->ypos++;
 		map->v += 64;
 	}	
