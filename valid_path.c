fcd54dfd5c13a9020d56c3e73b610c38223a8dbd
Ahmed Hassan <digiator2020@gmail.com>
2023-02-12 23:18:44 +0400

    path

--- valid_path.c
+++ valid_path.c
@@ -6,7 +6,7 @@
 /*   By: ahassan <ahassan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 16:49:52 by ahassan           #+#    #+#             */
-/*   Updated: 2023/02/12 23:04:41 by ahassan          ###   ########.fr       */
+/*   Updated: 2023/02/12 23:18:25 by ahassan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,13 @@
 void	first_depth(int py, int px, t_map *path)
 {
 	int x;
- 	// ft_printf("%c %d %d\n", path->map_dup[py][px], py, px);
+ 	ft_printf("%c %d %d\n", path->map_dup[py][px], py, px);
+	if(path->map_dup[py][px] == '1' || path->map_dup[py][px] == 'M')
+		return ;	
 	static int i = 1;
-	first_depth(px + 1, py, path);
-	// ft_printf("------------px+1 %d\n", i++);
-	first_depth(px - 1, py, path);
-	// ft_printf("------------px-1 %d\n", i++);
-	first_depth(px, py - 1, path);
-	// ft_printf("------------py-1 %d\n", i++);
-	first_depth(px, py + 1, path);
-	// ft_printf("------------py-1 %d\n", i++);
+	path->map_dup[py][px] = 'M';
+	first_depth(py, px+1, path);
+
 }
 
 void valid_path(t_map *map)
@@ -45,5 +42,17 @@ void valid_path(t_map *map)
 	}
 	map->dupc_cnt = map->c_cnt;
 	map->dupe_cnt = map->e_cnt;
-	first_depth(map->p_xpos, map->p_ypos, map);
+	first_depth(map->p_ypos, map->p_xpos, map);
+	y = 0;
+	while(y < map->y)
+	{
+		x = 0;
+		while (x < map->x)
+		{
+			ft_printf("%c", map->map_dup[y][x]);
+			x++;			
+		}
+		ft_printf("\n");
+		y++;
+	}
 }
