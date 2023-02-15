2d2b79c90b5616e138b76685c4c68f3f7df6d5cd
Ahmed Hassan <digiator2020@gmail.com>
2023-02-15 16:04:31 +0400

    total moves in exit

--- player_moves.c
+++ player_moves.c
@@ -6,7 +6,7 @@
 /*   By: ahassan <ahassan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 15:02:57 by ahassan           #+#    #+#             */
-/*   Updated: 2023/02/14 18:50:32 by ahassan          ###   ########.fr       */
+/*   Updated: 2023/02/15 16:03:30 by ahassan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	move_player_right(t_map *map)
 	if (map->map[map->p_ypos][map->p_xpos + 1] == '0'
 		|| map->map[map->p_ypos][map->p_xpos + 1] == 'C')
 	{
-		ft_printf("total moves %d\n", ++map->total_moves);
+		ft_printf("Total Moves %d\n", ++map->total_moves);
 		if (map->map[map->p_ypos][map->p_xpos + 1] == 'C')
 			map->c_cnt--;
 		++map->p_xpos;
@@ -29,7 +29,10 @@ void	move_player_right(t_map *map)
 	else if (map->map[map->p_ypos][map->p_xpos + 1] == 'E')
 	{
 		if (!map->c_cnt)
+		{
+			ft_printf("Total Moves %d\n", ++map->total_moves);
 			(x_click(map));
+		}
 	}
 }
 
@@ -38,7 +41,7 @@ void	move_player_left(t_map *map)
 	if (map->map[map->p_ypos][map->p_xpos - 1] == '0'
 		|| map->map[map->p_ypos][map->p_xpos - 1] == 'C')
 	{
-		ft_printf("total moves %d\n", ++map->total_moves);
+		ft_printf("Total Moves %d\n", ++map->total_moves);
 		if (map->map[map->p_ypos][map->p_xpos - 1] == 'C')
 			map->c_cnt--;
 		--map->p_xpos;
@@ -50,7 +53,10 @@ void	move_player_left(t_map *map)
 	else if (map->map[map->p_ypos][map->p_xpos - 1] == 'E')
 	{
 		if (!map->c_cnt)
+		{
+			ft_printf("Total Moves %d\n", ++map->total_moves);
 			(x_click(map));
+		}
 	}
 }
 
@@ -59,7 +65,7 @@ void	move_player_up(t_map *map)
 	if (map->map[map->p_ypos - 1][map->p_xpos] == '0' || map->map[map->p_ypos
 		- 1][map->p_xpos] == 'C')
 	{
-		ft_printf("total moves %d\n", ++map->total_moves);
+		ft_printf("Total Moves %d\n", ++map->total_moves);
 		if (map->map[map->p_ypos - 1][map->p_xpos] == 'C')
 			map->c_cnt--;
 		--map->p_ypos;
@@ -71,7 +77,10 @@ void	move_player_up(t_map *map)
 	else if (map->map[map->p_ypos - 1][map->p_xpos] == 'E')
 	{
 		if (!map->c_cnt)
+		{
+			ft_printf("Total Moves %d\n", ++map->total_moves);
 			(x_click(map));
+		}
 	}
 }
 
@@ -80,7 +89,7 @@ void	move_player_down(t_map *map)
 	if (map->map[map->p_ypos + 1][map->p_xpos] == '0' || map->map[map->p_ypos
 		+ 1][map->p_xpos] == 'C')
 	{
-		ft_printf("total moves %d\n", ++map->total_moves);
+		ft_printf("Total Moves %d\n", ++map->total_moves);
 		if (map->map[map->p_ypos + 1][map->p_xpos] == 'C')
 			map->c_cnt--;
 		++map->p_ypos;
@@ -92,6 +101,9 @@ void	move_player_down(t_map *map)
 	else if (map->map[map->p_ypos + 1][map->p_xpos] == 'E')
 	{
 		if (!map->c_cnt)
+		{
+			ft_printf("Total Moves %d\n", ++map->total_moves);
 			(x_click(map));
+		}
 	}
 }
